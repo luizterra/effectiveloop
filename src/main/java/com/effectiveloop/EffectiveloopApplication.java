f958b38515b689e7ac4d686404f83442da9d1021
Luiz Terra <luizterra@hotmail.com>
2018-09-29 00:38:13 -0600

    Initial Project Commit.

---
+++ src/main/java/com/effectiveloop/EffectiveloopApplication.java
@@ -0,0 +1,12 @@
+package com.effectiveloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveloopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveloopApplication.class, args);
+    }
+}
