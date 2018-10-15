eb1f6a9689b1061e203a43855ff8baa4c754de4a
Luiz Terra <luizterra@hotmail.com>
2018-10-15 17:07:14 -0600

    test

--- src/main/java/com/effectiveloop/web/controllers/IndexController.java
+++ src/main/java/com/effectiveloop/web/controllers/IndexController.java
@@ -1,4 +1,13 @@
 package com.effectiveloop.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
 }
